e2558c91b6a0f53f32adadc2f491d11e20cb2be2
Rohit Sirse <rohitsirse560@gmail.com>
2025-11-28 16:28:09 +0530

    updated client and server

---
+++ client/src/App.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home.jsx' 
+import Blog from './pages/Blog.jsx'
+import Layout from './pages/admin/Layout.jsx'
+import Dasboard from './pages/admin/Dasboard.jsx'
+import AddBlog from './pages/admin/AddBlog.jsx'
+import ListBlog from './pages/admin/ListBlog.jsx'
+import Comments from './pages/admin/Comments.jsx'
+import Login from './components/admin/Login.jsx'
+import 'quill/dist/quill.snow.css'
+
+const App = () => {
+  return (
+    <div>
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/blog/:id' element={<Blog/>} />
+        <Route path='/admin/*' element={true ? <Layout/> : <Login/>}>
+         <Route index element={<Dasboard/>}/>
+         <Route path='addBlog' element={<AddBlog/>}/>
+         <Route path='listBlog' element={<ListBlog/>}/>
+         <Route path='comments' element={<Comments/>}/>
+        </Route>
+      </Routes>
+    </div>
+  )
+}
+
+export default App
